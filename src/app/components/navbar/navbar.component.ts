1adeee6f6b5f05752ef2034abe3a12b62120cadc
Ezequiel Weiss <38471904+ezeweiss@users.noreply.github.com>
2022-11-01 23:15:23 -0300

    clean

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  estaActivo!: Usuario;
   constructor() { }
 
   ngOnInit(): void {
