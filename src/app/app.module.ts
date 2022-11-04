9ea930c4714108d5f45916c3111a14c07a32a542
Ezequiel Weiss <38471904+ezeweiss@users.noreply.github.com>
2022-11-04 12:02:27 -0300

    arreglos en carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
-import { NavbarComponent } from './navbar/navbar.component';
-import { VinoComponent } from './vino/vino.component';
-import { BebidasComponent } from './bebidas/bebidas.component';
 import {MatCardModule} from '@angular/material/card';
-import { LoginComponent } from './login/login.component';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule} from '@angular/material/input';
-import { WhiskyComponent } from './whisky/whisky.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { VinoComponent } from './components/vino/vino.component';
+import { BebidasComponent } from './components/bebidas/bebidas.component';
+import { WhiskyComponent } from './components/whisky/whisky.component';
+import { AuthComponent } from './components/auth/auth.component';
+
 
 @NgModule({
   declarations: [
@@ -26,8 +27,8 @@ import { WhiskyComponent } from './whisky/whisky.component';
     NavbarComponent,
     VinoComponent,
     BebidasComponent,
-    LoginComponent,
-    WhiskyComponent
+    WhiskyComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
