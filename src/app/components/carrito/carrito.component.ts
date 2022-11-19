1c587dd86640353436ec388769c328f7e95f3a52
Ezequiel Weiss <38471904+ezeweiss@users.noreply.github.com>
2022-11-18 22:38:32 -0300

    validaciones al comprar carrito

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -1,28 +1,44 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Bebida } from 'src/app/models/bebida';
 import { CarritoService } from 'src/app/services/carrito.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
   styleUrls: ['./carrito.component.css']
 })
-export class CarritoComponent {
+export class CarritoComponent implements OnInit, DoCheck{
   bebidas: Bebida[] = []
-  
+  isLogged!: Boolean;
   constructor(
     protected router: Router,
     protected http: HttpClient,
     protected route: ActivatedRoute,
     private toastr: ToastrService,
-    public carritoService: CarritoService
+    public carritoService: CarritoService,
+    private cookieService: CookieService
   ) {}
 
   ngOnInit(){
     this.bebidas = this.carritoService.getBebidas()
     console.log(this.bebidas)
+  }
+
+  ngDoCheck(): void {
+    this.isLogged = this.cookieService.check("token")
+  }
+
+  comprar(){
+    if(this.bebidas.length > 0){
+      this.toastr.success("Su compra ha sido éxitosa");   
+      this.router.navigate(["/"])
+    }
+    else{
+    this.toastr.error("El carrito está vacío")
+    }
   }
 }
